5bbe856471277acc1f20abc8eb755394bbbde050
Felipe Rossano Ferreira <57961089+feliperossanoferreira@users.noreply.github.com>
2019-12-29 13:49:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+function clicar() {
+    document.getElementById('des').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ";
+}
+
+function clicar1() {
+    document.getElementById('des').innerHTML = "adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ";
+}
+
+function clicar2() {
+    document.getElementById('des').innerHTML = "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ";
+}
+
